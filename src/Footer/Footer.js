4f0c372725b158174d0812461d1a6b943b9a17e5
Emily Moore <efm0004@gmail.com>
2019-09-24 10:37:27 -0500

    add header and footer

---
+++ src/Footer/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <p>I am a footer</p>
+    )
+}
+
+export default Footer
---
+++ src/Nav/Nav.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Nav = () => {
+    return (
+        <h3>I am a nav bar!</h3>
+    )
+}
+
+export default Nav
